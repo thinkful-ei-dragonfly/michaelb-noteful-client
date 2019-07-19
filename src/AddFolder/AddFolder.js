59fceb16a710f5d5d15a5362ed176d0d8c056a5e
michaelbravo777 <michael.bravo777@gmail.com>
2019-07-19 00:32:37 -0400

    added validation

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
+import ValidationError from '../ValidationError'
 import config from '../config'
 import './AddFolder.css'
 
 export default class AddFolder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      formValid: false,
+      validationMessages: {
+        name: '',
+      }
+    }
+  }
+
   static defaultProps = {
     history: {
       push: () => { }
     },
   }
+
+  updateName(name) {
+    this.setState({ name }, () => { this.validateName(name) });
+  }
+
   static contextType = ApiContext;
 
   handleSubmit = e => {
@@ -38,6 +55,38 @@ export default class AddFolder extends Component {
       })
   }
 
+  validateName(fieldValue) {
+    const fieldErrors = {...this.state.validationMessages};
+    let hasError = false;
+
+    fieldValue = fieldValue.trim();
+    if (fieldValue.length === 0) {
+      fieldErrors.name = 'Name is required';
+      hasError = true;
+    } else {
+      if (fieldValue.length < 3) {
+        fieldErrors.name = 'Name must be at least 3 characters long';
+        hasError = true;
+      } else {
+        fieldErrors.name = '';
+        hasError = false;
+      }
+    }
+
+    this.setState({
+      validationMessages: fieldErrors,
+      nameValid: !hasError
+    }, () => {
+      this.validateForm();
+    });
+  }
+
+  validateForm = () => {
+    this.setState({
+        formValid: this.state.nameValid
+    });
+  }
+
   render() {
     return (
       <section className='AddFolder'>
@@ -47,14 +96,20 @@ export default class AddFolder extends Component {
             <label htmlFor='folder-name-input'>
               Name
             </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
+            <input
+              type='text'
+              id='folder-name-input'
+              name='folder-name'
+              onChange={e => this.updateName(e.target.value)}
+              />
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button disabled={!this.state.formValid} type='submit'>
               Add folder
             </button>
           </div>
         </NotefulForm>
+        <ValidationError hasError={!this.state.nameValid} message={this.state.validationMessages.name} />
       </section>
     )
   }
